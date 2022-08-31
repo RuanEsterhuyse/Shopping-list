5846c34f1dbdc077fb75a66e1527f047ae1c22a0
Ruan Esterhuyse <ruan.esterhuyse@gmail.com>
2022-08-31 11:13:26 -0400

    make change

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function remove(e){
 
  let target = e.target;
 
- if(target.className =='delete'){  // bubbling
+ if(target.className =='delete'){  
      let li = target.parentElement; 
      li.remove();
  }
@@ -45,7 +45,7 @@ formAdd.addEventListener('submit', (e) =>{
  li.appendChild(groceryName);
  li.appendChild(deleteButton);
 
- //attach our newly created element to the DOM 
+ //attach newly created element to the DOM 
  ul.appendChild(li);
  //add text
  groceryName.textContent = text;
@@ -65,27 +65,24 @@ SEARCH.addEventListener('keyup', (e) => {
  let text = e.target.value.toLowerCase();
 let groceryList = document.querySelector('#grocery-list ul');
 let groceries = groceryList.getElementsByTagName('li');
-// need to convert the groceries to an array, beacuse it's an HTMLCollection, so we can access the forEach method. Can use JS's Array.from()
+// need to convert the groceries to an array, beacuse it's an HTMLCollection
 let groceriesArray = Array.from(groceries);
 // loop through each grocery item
 groceriesArray.forEach((grocery) =>{
     let groceryName = grocery.firstElementChild.textContent;
-    // convert all of our text into lower case 
+    
     groceryNameLower = groceryName.toLowerCase(); 
     if(groceryNameLower.indexOf(text) == -1){
      grocery.style.display = 'none'
     } else {
         grocery.style.display = 'block';
     }
-    // does it exist in grocery name 
+  
 
 
 })
 });
 
-// indexOf(searchItem, startingPoint);
-// it only exists for 2 data types
-// Arrays and strings 
 
 
 // +++++++++++++++++++++++TABS++++++++++++++++++++++++++
@@ -97,9 +94,9 @@ let panels = document.querySelectorAll('.panel');
 let selectedPanel = null;
 
 headings.addEventListener('click', (e) =>{
-    // let's find out which <li> tag trigerred the event
-    let target = e.target; // tells us which item trigerred that event
-    // lets use our dataset to get our data value.. we've called ours 'clicked' but you can call it whatever you like 
+   
+    let target = e.target; 
+   
     let dataAttribute = e.target.dataset.clicked; 
 
     if(target.tagName == "LI"){
@@ -110,9 +107,9 @@ headings.addEventListener('click', (e) =>{
         selectedPanel = target;
         selectedPanel.classList.toggle('selected');
 
-        // find the panel that we want to show
+    
         let targetPanel = document.querySelector(dataAttribute);
-        // now we need to determine whether the panel currently selected iss the one diplayed - remember a nodelist has access to the forEach() method but not an html collection need to convert it to an array
+      
         panels.forEach((panel) =>{
             if(panel == targetPanel){
                 panel.classList.add('active');
@@ -124,7 +121,7 @@ headings.addEventListener('click', (e) =>{
     }
 });
 
-// lets deal with our button to display answer 
+
 let answerButton = document.getElementById('showAnswer');
 answerButton.addEventListener('click', answer);
 
